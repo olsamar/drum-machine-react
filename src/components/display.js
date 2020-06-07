70bf2d5c2052e7745345a10d7701e5c55edab2ff
Mollie-S <olcha.olcha@gmail.com>
2020-06-08 00:22:41 +0200

    Added the Display component.

--- src/components/display.js
+++ src/components/display.js
@@ -1,10 +1,11 @@
 import React from "react";
-import Key from "./key";
 
-function Display() {
+function Display(props) {
+  // console.log(props);
+
   return (
     <div id="display">
-      <Key />
+      <p>{props.id}</p>
     </div>
   );
 }
