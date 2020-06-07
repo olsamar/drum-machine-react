c7ffcc9aab0c7fd16771f0d850000ab330f16569
Mollie-S <olcha.olcha@gmail.com>
2020-06-08 00:22:18 +0200

    Created drum pad elements.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,78 @@
 import React from "react";
 import Header from "./components/header";
 import Display from "./components/display";
+import DrumPad from "./components/drumpad";
 
 import "./App.css";
 
+const drumPadElements = [
+  {
+    id: "clap",
+    drumPadKey: "Q",
+    audiosource: "clap.wav",
+    className: "drum-pad",
+  },
+  {
+    id: "hihat",
+    drumPadKey: "W",
+    audiosource: "hihat.wav",
+    className: "drum-pad",
+  },
+  {
+    id: "kick",
+    drumPadKey: "E",
+    audiosource: "kick.wav",
+    className: "drum-pad",
+  },
+  {
+    id: "boom",
+    drumPadKey: "A",
+    audiosource: "boom.wav",
+    className: "drum-pad",
+  },
+  {
+    id: "openhat",
+    drumPadKey: "S",
+    audiosource: "openhat.wav",
+    className: "drum-pad",
+  },
+  {
+    id: "ride",
+    drumPadKey: "D",
+    audiosource: "ride.wav",
+    className: "drum-pad",
+  },
+  {
+    id: "snare",
+    drumPadKey: "Z",
+    audiosource: "snare.wav",
+    className: "drum-pad",
+  },
+  {
+    id: "tink",
+    drumPadKey: "X",
+    audiosource: "tink.wav",
+    className: "drum-pad",
+  },
+  { id: "tom", drumPadKey: "C", audiosource: "tom.wav", className: "drum-pad" },
+];
+
 function App() {
+  const drumPad = drumPadElements.map((element) => (
+    <DrumPad
+      key={element.drumPadKey}
+      drumPadKey={element.drumPadKey}
+      id={element.id}
+      audiosource={element.audiosource}
+    />
+  ));
+  console.log(drumPadElements.id);
+
   return (
     <div id="drum-machine">
       <Header />
       <Display />
+      <div id="drumPadPanel">{drumPad}</div>;
     </div>
   );
 }
